21c5f80378727608e64908178aafd67c18d64a46
user <user@DESKTOP-B0JEB9K>
2025-03-20 20:50:20 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
